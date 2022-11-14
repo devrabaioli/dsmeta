5a6e9dae6cd8b4eab823f91403cb2dc3e205b978
Thiago Rabaioli <thiagorabaioli@MBP-de-Thiago.lan>
2022-11-14 13:48:08 +0000

    First Commit

---
+++ backend/src/main/java/dev/rabaioli/desmeta/DesmetaApplication.java
@@ -0,0 +1,13 @@
+package dev.rabaioli.desmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/rabaioli/desmeta/DesmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rabaioli.desmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
