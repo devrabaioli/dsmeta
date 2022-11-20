7f251b8367ce9be229a62ba9be587dc0797731b4
Thiago Rabaioli <thiagorabaioli@MBP-de-Thiago.lan>
2022-11-20 18:44:27 +0000

    Date select

--- backend/src/main/java/dev/rabaioli/desmeta/controlleres/SaleController.java
+++ backend/src/main/java/dev/rabaioli/desmeta/controlleres/SaleController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.rabaioli.desmeta.entities.Sale;
@@ -21,8 +22,11 @@ public class SaleController {
 	private SaleService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public Page<Sale> findSales(Pageable page){
-		return service.findSales(page);
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "" ) String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
+			Pageable page){
+		return service.findSales(minDate, maxDate, page);
 	}
 
 }
--- backend/src/main/java/dev/rabaioli/desmeta/services/SaleService.java
+++ backend/src/main/java/dev/rabaioli/desmeta/services/SaleService.java
@@ -1,5 +1,8 @@
 package dev.rabaioli.desmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +19,14 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repo;
 	
-	public Page<Sale> findSales(Pageable pageable) {
-		return repo.findAll(pageable);
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate); 
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		return repo.findSales(min,max,pageable);
 		
 		
 	}
